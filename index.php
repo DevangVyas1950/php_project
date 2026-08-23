b579ae9842fd332b4f589e9bc42ad2145a89dc81
DevangVyas1950 <devuvyas1950@gmail.com>
2026-08-24 02:43:07 +0530

    Fix comment in index.php
    Corrected a comment in the PHP file.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php  we were only 11
+<?php  we were only 11 opps aa i got bababa
 require_once 'config/constants.php';
 if (is_logged_in()) {
     if (is_admin()) redirect('admin/dashboard.php');
